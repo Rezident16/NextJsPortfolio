0a2061aa3e1706aa9f6e1b3e23cad712648933a3
Andrei Vorobev <andrey.vorob1995@gmail.com>
2024-08-07 13:29:59 -0400

    footer is updated

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -6,31 +6,27 @@ import Image from "next/image";
 const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
-      <div className="container p-12 flex justify-between">
+      <div className="container p-12 flex flex-col md:flex-row justify-between">
         <div className="flex flex-col items-start text-slate-100">
-          <p className="flex flex-row">
-            <p className="flex flex-row gap-2">
-              <Image src={EmailIcon} />
-              <div>:</div>
-            </p>
+          <div className="flex flex-row gap-2">
+            <Image src={EmailIcon} />
+            <div>:</div>
             <a
               href="mailto:andrey.vorob1995@gmail.com"
               className="hover:text-blue-600 ml-4"
             >
               andrey.vorob1995@gmail.com
             </a>
-          </p>
-          <p className="flex flex-row">
-            <p className="flex flex-row gap-2">
-              <Image src={PhoneIcon} />
-              <div>:</div>
-            </p>
+          </div>
+          <div className="flex flex-row gap-2 mt-4 md:mt-0">
+            <Image src={PhoneIcon} />
+            <div>:</div>
             <a href="tel:+14156911018" className="hover:text-blue-600 ml-4">
               +1 (415) 691-1018
             </a>
-          </p>
+          </div>
         </div>
-        <p className="text-slate-600">All rights reserved.</p>
+        <p className="text-slate-600 mt-4 md:mt-0">All rights reserved.</p>
       </div>
     </footer>
   );
